d6442621cbaba7754f5c125a32c531f7f0913920
Erik Hergins <hergins1@hotmail.com>
2019-06-30 21:35:31 -0500

    creating heroku connection

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -36,17 +36,17 @@ router.put('/api/updateburger/:id', function(req, res){
     });
 });
 
-router.delete("/api/burger/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
+// router.delete("/api/burger/:id", function(req, res) {
+//     var condition = "id = " + req.params.id;
   
-    burger.delete(condition, function(result) {
-      if (result.affectedRows == 0) {
-        return res.status(404).end();
+//     burger.delete(condition, function(result) {
+//       if (result.affectedRows == 0) {
+//         return res.status(404).end();
         
-      } else {
-        res.status(200).end();
-      }
-    });
-  });
+//       } else {
+//         res.status(200).end();
+//       }
+//     });
+//   });
 
   module.exports = router;
